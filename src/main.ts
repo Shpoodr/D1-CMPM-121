0bc9dafd24071a7b6143130c641b2b78afcd3347
Shpoodr <116927445+Shpoodr@users.noreply.github.com>
2025-10-08 05:43:09 +0000

    Made it so the button will Increment the counter.

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import potatoImage from "./potatoImage.png";
 import "./style.css";
 
-//let counter: number = 0;
+let counter: number = 0;
 document.body.innerHTML = `
   <p> counter: <span id="counter">0</span></p>
   <button id="button"> <img src="${potatoImage}"</button>
 `;
 
-//const button = document.getElementById("button")!;
-//const counterElement = document.getElementById("counter")!;
+const button = document.getElementById("button")!;
+const counterElement = document.getElementById("counter")!;
+
+button.addEventListener("click", () => {
+  counter += 1;
+  counterElement.textContent = counter.toString();
+});
