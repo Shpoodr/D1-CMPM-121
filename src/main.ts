57a05a22b257d3f5a2d7613aa86a71c5d9a1c051
Shpoodr <116927445+Shpoodr@users.noreply.github.com>
2025-10-09 22:00:37 +0000

    Added the fractional increase for step 4. I miss interpreted what I needed to do before commiting the last code.

--- src/main.ts
+++ src/main.ts
@@ -4,34 +4,36 @@ import "./style.css";
 //important variables used throughout the code
 let counter: number = 0;
 let lastTime = 0;
-let lastIncrement = 0;
-const intervalSpeed = 1000; //1000 milliseconds = 1 second
+//let lastIncrement = 0;
+//const intervalSpeed = 1000; //1000 milliseconds = 1 second
+let counterIncreaseAmount = 0;
 
 document.body.innerHTML = `
   <p> counter: <span id="counter">0</span></p>
   <button id="button"> <img src="${potatoImage}"</button>
 `;
 
-function incrementCounter() {
-  counter += 1;
-  counterElement.textContent = counter.toString();
+function incrementCounter(increaseAmount: number) {
+  counter += increaseAmount;
+  counterElement.textContent = counter.toFixed(2);
 }
 
 function getAnimationInterval(timeStamp: number) {
   //this means timeStamp is 0 or the first frame
   if (timeStamp === 0) {
     lastTime = timeStamp;
-    incrementCounter();
+    incrementCounter(counterIncreaseAmount);
     return;
   }
   const deltaTime = timeStamp - lastTime;
   lastTime = timeStamp;
-
-  lastIncrement += deltaTime;
+  counterIncreaseAmount = deltaTime / 1000;
+  incrementCounter(counterIncreaseAmount);
+  /*lastIncrement += deltaTime;
   if (lastIncrement >= intervalSpeed) {
     incrementCounter();
     lastIncrement -= intervalSpeed;
-  }
+  }*/
   requestAnimationFrame(getAnimationInterval);
 }
 
@@ -39,6 +41,8 @@ const button = document.getElementById("button")!;
 const counterElement = document.getElementById("counter")!;
 
 //comment
-button.addEventListener("click", incrementCounter);
+button.addEventListener("click", () => {
+  incrementCounter(1);
+});
 //setInterval(incrementCounter, 1000);
 requestAnimationFrame(getAnimationInterval);
