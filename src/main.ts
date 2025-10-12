1debddd74524306dd5dc0a4280a0141aa55b5195
Shpoodr <116927445+Shpoodr@users.noreply.github.com>
2025-10-12 03:08:22 +0000

    Step 6 Completed. Added multiple upgrades availible for the player. Also added a status variable that tracks how many potatos you are getting from the items that you buy. Finally I changed some of the names for the items to fit the theme a little better.

--- src/main.ts
+++ src/main.ts
@@ -3,21 +3,25 @@ import "./style.css";
 
 //important variables used throughout the code
 let counter: number = 0;
+let status = 0;
 let lastTime = 0;
 //let lastIncrement = 0;
 //const intervalSpeed = 1000; //1000 milliseconds = 1 second
 let counterIncreaseAmount = 0;
 let counterGrowthRate = 0;
-let clickGrowthRate = 1;
+//let clickGrowthRate = 1;
 //cost things
-let firstUpgradeCost = 10;
-let clickerUpgradeCost = 100;
+const firstUpgradeCost = 10;
+const secondUpgradeCost = 100;
+const thirdUpgradeCost = 1000;
 
 document.body.innerHTML = `
-  <p> counter: <span id="counter">0</span></p>
+  <p> Total Potatos: <span id="counter">0</span></p>
+  <p> Per Second: <span id ="status">${status}/s</span></p>
   <button id="button"> <img src="${potatoImage}"</button>
-  <button id="button2">Potatoe Farm cost: ${firstUpgradeCost}</button>
-  <button id="button3">Upgrade Clicker cost: ${clickerUpgradeCost}</button>
+  <button id="button2">Garden Pot cost: ${firstUpgradeCost}</button>
+  <button id="button3">Potato Patch cost: ${secondUpgradeCost}</button>
+  <button id="button4">A sorta working tractor cost: ${thirdUpgradeCost}</button>
 `;
 
 function incrementCounter(increaseAmount: number) {
@@ -35,6 +39,9 @@ function getAnimationInterval(timeStamp: number) {
   const deltaTime = timeStamp - lastTime;
   lastTime = timeStamp;
   counterIncreaseAmount = (deltaTime / 1000) * counterGrowthRate;
+  status = counterGrowthRate;
+  statusElement.textContent = `${status.toFixed(2)}/s`;
+
   incrementCounter(counterIncreaseAmount);
   /*lastIncrement += deltaTime;
   if (lastIncrement >= intervalSpeed) {
@@ -45,32 +52,43 @@ function getAnimationInterval(timeStamp: number) {
 }
 
 const button = document.getElementById("button")!;
-const button2 = document.getElementById("button2")!;
-const button3 = document.getElementById("button3")!;
+const pot = document.getElementById("button2")!;
+const patch = document.getElementById("button3")!;
+const tractor = document.getElementById("button4")!;
+//const button4 = document.getElementById("button4")!;
 const counterElement = document.getElementById("counter")!;
+const statusElement = document.getElementById("status")!;
 
 //comment
 button.addEventListener("click", () => {
-  incrementCounter(1 * clickGrowthRate);
+  incrementCounter(1);
 });
 
-button2.addEventListener("click", () => {
+pot.addEventListener("click", () => {
   if (counter >= firstUpgradeCost) {
-    counterGrowthRate += 1;
+    counterGrowthRate += 0.1;
     counter -= firstUpgradeCost;
-    firstUpgradeCost *= 1.15;
-    button2.textContent = `Potatoe Farm cost: ${firstUpgradeCost.toFixed(2)}`;
+    //firstUpgradeCost *= 1.15;
+    pot.textContent = `Garden Pot cost: ${firstUpgradeCost.toFixed(2)}`;
   }
 });
-button3.addEventListener("click", () => {
-  if (counter >= clickerUpgradeCost) {
-    clickGrowthRate += 1;
-    counter -= clickerUpgradeCost;
-    clickerUpgradeCost *= 5;
-    button3.textContent = `Upgrade Clicker cost: ${
-      clickerUpgradeCost.toFixed(2)
+patch.addEventListener("click", () => {
+  if (counter >= secondUpgradeCost) {
+    counterGrowthRate += 2;
+    counter -= secondUpgradeCost;
+    //secondUpgradeCost *= 1.15;
+    patch.textContent = `Potato Patch cost: ${secondUpgradeCost.toFixed(2)}`;
+  }
+});
+tractor.addEventListener("click", () => {
+  if (counter >= thirdUpgradeCost) {
+    counterGrowthRate += 10;
+    counter -= thirdUpgradeCost;
+    //thirdUpgradeCost *= 1.15;
+    tractor.textContent = `A sorta working tractor cost: ${
+      thirdUpgradeCost.toFixed(2)
     }`;
   }
 });
-//setInterval(incrementCounter, 1000);
+
 requestAnimationFrame(getAnimationInterval);
