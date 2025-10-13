b982f9e1a11347ab238dce53815391aadc99b434
Shpoodr <116927445+Shpoodr@users.noreply.github.com>
2025-10-13 01:06:52 +0000

    step 10 completed. Added two more items to but as well as added descriptions to the buttons. May add more because I want to make the game a little prettier but we will see.

--- src/main.ts
+++ src/main.ts
@@ -14,10 +14,12 @@ let counterGrowthRate = 0;
 document.body.innerHTML = `
   <p> Total Potatos: <span id="counter">0</span></p>
   <p> Per Second: <span id ="status">${status}/s</span></p>
-  <button id="button"> <img src="${potatoImage}"</button>
+  <button id="button"> <img src="${potatoImage}" class="potato-img"</button>
   <button id="button2">Garden Pot cost: 10</button>
   <button id="button3">Potato Patch cost: 100</button>
   <button id="button4">A sorta working tractor cost: 1000</button>
+  <button id="button5">Automatic farm cost: 10000</button>
+  <button id="button6">Hydoponic Farm Lab cost: 1000000</button>
 `;
 
 interface Upgrade {
@@ -25,6 +27,7 @@ interface Upgrade {
   cost: number;
   growthRateIncrease: number;
   button: HTMLElement;
+  description: string;
 }
 
 const upgrades: Upgrade[] = [
@@ -33,18 +36,35 @@ const upgrades: Upgrade[] = [
     cost: 10,
     growthRateIncrease: 0.1,
     button: document.getElementById("button2")!,
+    description: "A small pot you can start growing potatos out of. Equivalent to 0.1 potatos per second.",
   },
   {
     name: "Potato Patch",
     cost: 100,
-    growthRateIncrease: 2,
+    growthRateIncrease: 1,
     button: document.getElementById("button3")!,
+    description: "A small patch of dirt to grow more potatos. Equivalent to 1 potato per second.",
   },
   {
     name: "A sorta working tractor",
     cost: 1000,
     growthRateIncrease: 10,
     button: document.getElementById("button4")!,
+    description: "A rusty tractor to help with the heavy lifting. Equivalent to 10 potatos per second.",
+  },
+  {
+    name: "Automatic farm",
+    cost: 10000,
+    growthRateIncrease: 50,
+    button: document.getElementById("button5")!,
+    description: "A farm that does everything by it self. Equivalent to 50 potatos per second.",
+  },
+  {
+    name: "Hydoponic Farm Lab",
+    cost: 1000000,
+    growthRateIncrease: 500,
+    button: document.getElementById("button6")!,
+    description: "A high tech farm that grows potatos at an insane rate. Equivalent to 500 potatos per second.",
   },
 ];
 
@@ -62,6 +82,10 @@ function upgrade(upgrade: Upgrade) {
 }
 
 upgrades.forEach(upgrade);
+upgrades.forEach((upg) => {
+  upg.button.title = upg.description;
+  upgrade(upg);
+});
 
 function incrementCounter(increaseAmount: number) {
   counter += increaseAmount;
